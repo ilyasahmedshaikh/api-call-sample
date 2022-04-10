7d4dde3c0a9d4a8d3b6fab6b21ac92a4b7174a81
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2022-04-10 23:49:28 +0500

    remove un necessary component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,11 @@ import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
